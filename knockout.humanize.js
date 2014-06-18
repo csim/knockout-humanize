a6d72ecb58ce8367cd9be0fcc74947f4b210319b
Clint Simon <me@clintsimon.com>
2014-06-18 13:26:23 -0700

    Initial Commit

---
+++ knockout.humanize.js
@@ -0,0 +1,137 @@
+﻿/* global ko, humanize */
+
+ko.bindingHandlers.number = {
+    update: function(element, valueAccessor, allBindingsAccessor)
+    {
+        var bindings = allBindingsAccessor();
+        var decimals = bindings.decimals || 0;
+        var decimalPoint = bindings.decimalPoint || '.';
+        var separator = bindings.separator || ',';
+
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.numberFormat(value,
+                        decimals,
+                        decimalPoint,
+                        separator);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+        //ko.bindingHandlers.html.update(element, getval);
+    }
+};
+
+ko.bindingHandlers.ordinal = {
+    update: function(element, valueAccessor)
+    {
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.ordinal(value);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+    }
+};
+
+ko.bindingHandlers.naturalday = {
+    update: function(element, valueAccessor, allBindingsAccessor)
+    {
+        var bindings = allBindingsAccessor();
+        var format = bindings.format || 'Y-m-d';
+
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.naturalDay(value, format);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+        //ko.bindingHandlers.html.update(element, getval);
+    }
+};
+
+ko.bindingHandlers.filesize = {
+    update: function(element, valueAccessor, allBindingsAccessor)
+    {
+        var bindings = allBindingsAccessor();
+        var kilo = bindings.kilo || 1024;
+        var decimals = bindings.decimals || 2;
+
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.filesize(value, kilo, decimals);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+        //ko.bindingHandlers.html.update(element, getval);
+    }
+};
+
+ko.bindingHandlers.linebreaks = {
+    update: function(element, valueAccessor)
+    {
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.linebreaks(value);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+    }
+};
+
+ko.bindingHandlers.nl2br = {
+    update: function(element, valueAccessor)
+    {
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.nl2br(value);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+    }
+};
+
+ko.bindingHandlers.truncate = {
+    update: function(element, valueAccessor, allBindingsAccessor)
+    {
+        var bindings = allBindingsAccessor();
+        var length = bindings.length || 100;
+
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.truncatechars(value, length);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+    }
+};
+
+ko.bindingHandlers.truncatewords = {
+    update: function(element, valueAccessor, allBindingsAccessor)
+    {
+        var bindings = allBindingsAccessor();
+        var length = bindings.length || 100;
+
+        var getval = function()
+        {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            return humanize.truncatewords(value, length);
+        };
+
+        ko.bindingHandlers.text.update(element, getval);
+        ko.bindingHandlers.value.update(element, getval);
+    }
+};
